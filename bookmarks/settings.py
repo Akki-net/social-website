ab6b9d1cef3b8ba62ba0f0594ec5b80f154a59d6
Akki-net <akashsharma.zak@gmail.com>
2024-02-28 21:41:11 +0530

    created a login view

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bookmarks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'account' / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
