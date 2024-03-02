e37ab473db6e153d81f9a517d9f2e864d89a61d5
Akki-net <akashsharma.zak@gmail.com>
2024-03-02 20:35:22 +0530

    extending the user model and installing pillow and serving media files

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
+class Profile(models.Model):
+    class User(models.TextChoices):
+        PATIENT = 'PT', 'Patient'
+        DOCTOR = 'DR', 'Doctor'
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    type_of_user = models.CharField(max_length=2, choices=User.choices, default=User.PATIENT)
+    address = models.TextField()
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    def _str_(self):
+        return f'Profile of {self.user.username}'
