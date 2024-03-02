32af80c8275922925feb8d4b44a6fb916cf01c87
Akki-net <akashsharma.zak@gmail.com>
2024-03-02 21:01:48 +0530

    creating migrations for profile model and using a custom user model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Profile
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'type_of_user', 'address', 'photo']
+    raw_id_fields = ['user']
