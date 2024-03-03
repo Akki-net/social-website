6cdefe33767e14f45773d4508431ad59cbaadb62
Akki-net <akashsharma.zak@gmail.com>
2024-03-03 13:07:46 +0530

    change password views

--- account/urls.py
+++ account/urls.py
@@ -7,7 +7,13 @@ urlpatterns = [
     # login / logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    # dashboard urls
+    path('', views.dashboard, name='dashboard'),
     path('edit/', views.edit, name='edit'),
+    # change password urls
+    path('password-change/', auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(),
+          name='password_change_done'),
 ]
