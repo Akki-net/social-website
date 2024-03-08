0cba6b5939e24324576203d0f7cebeb8d918b5b4
Akki-net <akashsharma.zak@gmail.com>
2024-03-08 11:49:10 +0530

    Customizing how models are displayed

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Post
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'type_of_user', 'address', 'photo']
     raw_id_fields = ['user']
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
