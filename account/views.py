7d679ff28aca177814413c3fbb71a8824ff06257
Akki-net <akashsharma.zak@gmail.com>
2024-03-08 10:17:12 +0530

    Preventing users from using an existing email

--- account/views.py
+++ account/views.py
@@ -57,7 +57,7 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-        update = True
+            update = True
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
